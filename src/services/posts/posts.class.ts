56b406cd3a046fc457db8c43683bbc7e11e32c8c
b-haytham <boussarsarhaytham@gmail.com>
2020-11-23 21:50:59 +0100

    save uploaded images

--- src/services/posts/posts.class.ts
+++ src/services/posts/posts.class.ts
@@ -1,7 +1,9 @@
-import { Id, Params } from '@feathersjs/feathers';
-import { Service, MongooseServiceOptions } from 'feathers-mongoose';
-import app from '../../app';
-import { Application } from '../../declarations';
+import { Id, Params } from "@feathersjs/feathers";
+import { Service, MongooseServiceOptions } from "feathers-mongoose";
+import app from "../../app";
+import { Application } from "../../declarations";
+
+import { promises } from "fs";
 
 export class Posts extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,12 +11,24 @@ export class Posts extends Service {
     super(options);
   }
 
-  create(data:any, params?: Params ) { 
+  //@ts-ignore
+  async create(data: any, params?: Params) {
+    const path = `http://192.168.1.17:3030/uploads/${data.data.name}`;
 
-    
+    try {
+      await promises.writeFile(
+        `./public/uploads/${data.data.name}`,
+        data.data.base64,
+        {
+          encoding: "base64",
+        }
+      );
 
-    return super.create(data, params)
-  } 
-
-
+      data.content = path;
+      delete data.data;
+      return super.create(data, params);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
