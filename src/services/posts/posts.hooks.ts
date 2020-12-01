07561a55668efa5fdf5c96b2fd87983986329899
b-haytham <boussarsarhaytham@gmail.com>
2020-12-01 02:20:23 +0100

    populate user data on posts response

--- src/services/posts/posts.hooks.ts
+++ src/services/posts/posts.hooks.ts
@@ -1,33 +1,61 @@
-import * as authentication from '@feathersjs/authentication';
-import { populate, preventChanges } from 'feathers-hooks-common';
-import insertId from '../../hooks/insertId';
-import IsAuthorized from '../../hooks/IsAuthorized';
+import * as authentication from "@feathersjs/authentication";
+import {
+  populate,
+  PopulateSchema,
+  preventChanges,
+} from "feathers-hooks-common";
+import insertId from "../../hooks/insertId";
+import IsAuthorized from "../../hooks/IsAuthorized";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
 
+const postSchema: Partial<PopulateSchema> = {
+  include: [
+    {
+      service: "users",
+      nameAs: "owner",
+      parentField: "owner",
+      childField: "_id",
+      query: {
+        $select: ["_id", "username", "avatar"],
+      },
+    },
+  ],
+};
+
 export default {
   before: {
-    all: [ authenticate('jwt')],
+    all: [authenticate("jwt")],
     find: [],
     get: [],
     create: [],
-    update: [IsAuthorized({serviceToProtect: 'posts', fieldToCheck: 'owner'}) ],
-    patch: [
-      IsAuthorized({serviceToProtect: 'posts', fieldToCheck: 'owner'}),
-      preventChanges(true, ...["owner", "comments"]) 
+    update: [
+      IsAuthorized({ serviceToProtect: "posts", fieldToCheck: "owner" }),
     ],
-    remove: [IsAuthorized({serviceToProtect: 'posts', fieldToCheck: 'owner'}) ]
+    patch: [
+      IsAuthorized({ serviceToProtect: "posts", fieldToCheck: "owner" }),
+      preventChanges(true, ...["owner", "comments"]),
+    ],
+    remove: [
+      IsAuthorized({ serviceToProtect: "posts", fieldToCheck: "owner" }),
+    ],
   },
 
   after: {
     all: [],
-    find: [],
+    find: [populate({ schema: postSchema })],
     get: [],
-    create: [insertId({serviceTargetName: 'users', idField: 'owner' , targetFieldToUpdate: 'posts'})],
+    create: [
+      insertId({
+        serviceTargetName: "users",
+        idField: "owner",
+        targetFieldToUpdate: "posts",
+      }),
+    ],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -37,6 +65,6 @@ export default {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
